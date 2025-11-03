f0fca1fb5972a4792835dde93b26781da876ce3e
vblyakherov <v.blyakherov@gmail.com>
2025-11-03 13:10:15 +0300

    Stop encoding default fields in JSON client

--- app/src/main/java/com/kubyshka/teacherworkspace/network/TeacherApiService.kt
+++ app/src/main/java/com/kubyshka/teacherworkspace/network/TeacherApiService.kt
@@ -67,7 +67,6 @@ interface TeacherApiService {
 private val json = Json {
     ignoreUnknownKeys = true
     isLenient = true
-    encodeDefaults = true
 }
 
 fun createTeacherApiService(): TeacherApiService {
